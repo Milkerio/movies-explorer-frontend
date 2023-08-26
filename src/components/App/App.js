9ea79558a2d8a67068540fc70e80f6188db80e42
Vladimir Metelev <metelev_vladimir@mail.ru>
2023-08-26 20:50:49 +0300

    убрал useLocation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
